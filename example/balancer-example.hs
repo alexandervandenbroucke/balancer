c6795c7b91414b2febd8081c48f967223e8707ce
Alexander Vandenbroucke <alexander.vandenbroucke@kuleuven.be>
2017-12-06 17:38:53 +0100

    Add "noprint" option to balancer-example.

--- example/balancer-example.hs
+++ example/balancer-example.hs
@@ -2,22 +2,26 @@ import System.Environment (getArgs)
 
 import qualified Balancer as B
 import qualified Greedy   as G
-import Types (Expense(..),Transfer,prettyTransfers)
+import Types (Expense(..),Transfer(MkTransfer),prettyTransfers)
+import Control.DeepSeq (NFData(rnf),deepseq)
+
+instance NFData a => NFData (Transfer a) where
+  rnf (MkTransfer from to amount) =
+    from `deepseq` to `deepseq` amount `deepseq` ()
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [n] -> go B.balance (read n)
-    ["flow",n]   -> go B.balance                  (read n)
-    ["greedy",n] -> go (\es -> G.balance es 0.01) (read n)
+    [n] -> prettyTransfers $ go B.balance (read n)
+    ["flow",n]    -> prettyTransfers $ go B.balance                  (read n)
+    ["greedy",n]  -> prettyTransfers $ go (\es -> G.balance es 0.01) (read n)
+    ["noprint",n] -> go (\es -> G.balance es 0.01) (read n) `deepseq` return ()
     _            -> do
       putStrLn "usage: main [flow | greedy] n"
 
-go :: ([Expense String] -> [Transfer String]) -> Int -> IO ()
-go balance n = do
-  let records = [MkExpense (show i) (100 * i2d i) | i <- [1..n]]
-  prettyTransfers (balance records)
+go :: ([Expense String] -> [Transfer String]) -> Int -> [Transfer String]
+go balance n = balance [MkExpense (show i) (100 * i2d i) | i <- [1..n]]
 
 i2d :: Int -> Double
 i2d = fromInteger . toInteger
